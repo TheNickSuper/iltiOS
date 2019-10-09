2f5bb1d856be7019c6f3f1fcd32dd2f2ec8881f9
TheNickSuper <52745074+TheNickSuper@users.noreply.github.com>
2019-10-09 22:57:39 +0500

    Create variables.js

---
+++ v0.0.3/variables.js
@@ -0,0 +1,7 @@
+var a = document.getElementById("text"); 
+var b, c, d, e, screenData, colorText, colorBackground;
+    screenData = null;
+    colorText = "green";
+    colorBackground = "black";
+var body = document.getElementsByTagName("body")[0];
+    var chat, jl, jhg, jlg, jgh = false;
